e494d8e87aa69c49e32a7379fe04ed947589b5cb
Imen Mlika <imenmlika18@gmail.com>
2026-01-09 10:29:59 +0100

    offline

--- app/src/main/java/com/example/movievault/MainActivity.kt
+++ app/src/main/java/com/example/movievault/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.movievault.ui.theme.MovieVaultTheme
 import com.example.movievault.navigation.AppNavHost
-
+import com.example.movievault.ui.theme.MovieVaultTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.firestore.PersistentCacheSettings
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setLocalCacheSettings(PersistentCacheSettings.newBuilder().build())
+            .build()
+
+        FirebaseFirestore.getInstance().firestoreSettings = settings
+
         enableEdgeToEdge()
+
         setContent {
-            MovieVaultTheme {
-                AppNavHost()
-            }
+            MovieVaultTheme { AppNavHost() }
         }
     }
 }
